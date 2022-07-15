9ca0231f041776d7c84898f416e88a63ef094a2c
zxzx93 <thddustn26@hanmail.net>
2022-07-15 17:17:24 +0900

    popular topics 버튼 선택 했을때 border 색상 추가

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -5,6 +5,9 @@ import { useRouter } from "next/router";
 import { topics } from "../utils/constants";
 
 const Discover = () => {
+  const router = useRouter();
+  const { topic } = router.query;
+
   return (
     <div className="xl:border-b-2 xl:order-gray-200 pb-6">
       <p className="text-gray-500 font-semibold m-3 mt-4 xl:block hidden">
@@ -12,14 +15,16 @@ const Discover = () => {
       </p>
 
       <div className="flex gap-3 flex-wrap">
-        {topics.map((value) => (
-          <Link href={`/?topic=${value.name}`} key={value.name}>
-            <div className="topicStyle">
-              <span className="font-blod t xl:text-md text-2xl ">
-                {value.icon}
+        {topics.map((item) => (
+          <Link href={`/?topic=${item.name}`} key={item.name}>
+            <div
+              className={topic === item.name ? "activeTopicStyle" : "topicStyle"}
+            >
+              <span className="font-blod t xl:text-md text-2xl">
+                {item.icon}
               </span>
               <span className="font-medium hidden text-md xl:block capitalize">
-                {value.name}
+                {item.name}
               </span>
             </div>
           </Link>
