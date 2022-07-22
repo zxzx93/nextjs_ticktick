c81dc216a266694a1343096189f9c79a36c6e986
zxzx93 <thddustn26@hanmail.net>
2022-07-22 18:26:58 +0900

    zustand authStore 만들고 유저정보 추가, 삭제하는 기능 추가

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,3 +2,14 @@ import create from "zustand";
 import { persist, devtools } from "zustand/middleware";
 import axios from "axios";
 
+const authStore = (set: any) => ({
+  userProfile: null,
+
+  //스토어에 유저 정보 추가 , 삭제
+  addUser: (user: any) => set({ userProfile: user }),
+  removeUser: () => set({ userProfile: null }),
+});
+
+const useAuthStore = create(devtools(persist(authStore, { name: "auth" })));
+
+export default useAuthStore;
