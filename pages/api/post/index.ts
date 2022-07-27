3ac9b075ede3e35ad5ef8366d4d5540fbff25a60
zxzx93 <thddustn26@hanmail.net>
2022-07-27 19:02:20 +0900

    비디오 업로드 api 추가

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -12,12 +12,11 @@ export default async function handler(
 
     const data = await client.fetch(query);
     res.status(200).json(data);
-  }
-  // else if (req.method === "POST") {
-  //   const doc = req.body;
+  } else if (req.method === "POST") {
+    const doc = req.body;
 
-  //   client.create(doc).then(() => {
-  //     res.status(200).json("video created");
-  //   });
-  // }
+    client.create(doc).then(() => {
+      res.status(201).json("video created");
+    });
+  }
 }
