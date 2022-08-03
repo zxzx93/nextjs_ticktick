023ba6ef878e11e21a51ad255d18677440fdfdd5
zxzx93 <thddustn26@hanmail.net>
2022-08-03 18:33:01 +0900

    비디오 댓글 기능  api 추가

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { uuid } from "uuidv4";
 
 import { client } from "../../../utils/client";
 import { postDetailQuery } from "../../../utils/queries";
@@ -13,5 +14,23 @@ export default async function handler(
 
     const data = await client.fetch(query);
     res.status(200).json(data[0]);
+  } else if (req.method === "PUT") {
+    const { userId, comment } = req.body;
+    const { id }: any = req.query; //postId
+
+    //MEMO: comments 뒤에 값 들어감
+    const data = await client
+      .patch(id)
+      .setIfMissing({ comments: [] })
+      .insert("after", "comments[-1]", [
+        {
+          _key: uuid(),
+          postedBy: { _type: "postedBy", _ref: userId },
+          comment,
+        },
+      ])
+      .commit();
+
+    res.status(200).json(data);
   }
 }
