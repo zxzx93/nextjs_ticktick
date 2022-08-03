3c33068908c6a59a4eade3b659213a7714decdc4
zxzx93 <thddustn26@hanmail.net>
2022-08-03 18:36:38 +0900

    비디오 디테일 댓글, 프로파일 페이지에 컨텐츠 없을때 나오는 컴포는트 생성

--- components/NoResult.tsx
+++ components/NoResult.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { NextPage } from "next";
+import { MdOutlineVideocamOff } from "react-icons/md";
+import { BiCommentX } from "react-icons/bi";
 
 interface Iprops {
   text: string;
+  emptyComment?: boolean;
 }
 
-const NoResult = ({ text }: Iprops) => {
-  return <div>NoResult</div>;
+const NoResult = ({ text, emptyComment }: Iprops) => {
+
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {emptyComment ? <BiCommentX /> : <MdOutlineVideocamOff />}
+      </p>
+      <p className="text-xl text-center">{text}</p>
+    </div>
+  );
 };
 
 export default NoResult;
